a40808ff14d41e4b62b9d7d2cd4f08ad0dbf334b
alexis62 <alexis.gressier62@gmail.com>
2018-02-15 18:36:57 +0100

    Import MVC Project

---
+++ model/PostManager.php
@@ -0,0 +1,28 @@
+<?php
+class PostManager
+{
+    public function getPosts()
+    {
+        $db = $this->dbConnect();
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+
+        return $req;
+    }
+
+    public function getPost($postId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+
+        return $post;
+    }
+
+
+    private function dbConnect()
+    {
+        $db = new PDO('mysql:host=db724281053.db.1and1.com;dbname=db724281053;charset=utf8', 'dbo724281053', 'Azerty12!');
+        return $db;
+    }
+}
