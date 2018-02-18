95f2181feb8930974ddf6372da9adf7a80f0d6bb
alexis62 <alexis.gressier62@gmail.com>
2018-02-18 17:58:56 +0100

    Add The Namespace

---
+++ controller/frontend.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once ('model/PostManager.php');
+require_once  ('model/CommentManager.php');
+
+class Frontend
+{
+
+    public function listPosts()
+    {
+        $postManager = new \Projet4\Model\PostManager(); // Création d'un objet
+        $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
+
+        require('view/frontend/listPostsView.php');
+    }
+
+    public function post()
+    {
+        $postManager = new \Projet4\Model\PostManager();
+        $commentManager = new \Projet4\Model\CommentManager();
+
+        $post = $postManager->getPost($_GET['id']);
+        $comments = $commentManager->getComments($_GET['id']);
+
+        require('view/frontend/postView.php');
+    }
+
+    public function addComment($postId, $author, $comment)
+    {
+        $commentManager = new \Projet4\Model\CommentManager();
+
+        $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+        if ($affectedLines === false) {
+            throw new \Exception('Impossible d\'ajouter le commentaire !');
+        } else {
+            header('Location: index.php?action=post&id=' . $postId);
+        }
+    }
+}
