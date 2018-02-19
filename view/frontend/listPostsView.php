3f3444c8eaf3c171dd94a27f67b43a8a50289f81
alexis62 <alexis.gressier62@gmail.com>
2018-02-19 12:45:28 +0100

    Add a script tinymce

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -2,7 +2,13 @@
 
 <?php ob_start(); ?>
 <h1>Mon super blog !</h1>
-<p>Derniers billets du blog :</p>
+<p>Derniers billets du bloge :</p>
+    <p><a href="/admin/index.php">Conexion</a></p>
+
+    <h1>TinyMCE Quick Start Guide</h1>
+    <form method="post">
+        <textarea id="mytextarea">Hello, World!</textarea>
+    </form>
 
 
 <?php
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,7 +3,14 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="public/css/style.css" rel="stylesheet" /> 
+        <link href="public/css/style.css" rel="stylesheet" />
+        <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=8rx5j5se2v96khf9x0zh982xdtwdsp1obbeht57c6jd81ng0"></script>
+        <script>
+            tinymce.init({
+                selector: '#mytextarea'
+            });
+        </script>
+
     </head>
         
     <body>
