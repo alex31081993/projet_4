633011c7a36800ef37140008eca4d3e2c7cc3855
alexis.gressier62@gmail.com <alexis.gressier62@gmail.com>
2018-03-01 13:34:44 +0100

    add files view update

---
+++ view/backend/updatePostView.php
@@ -0,0 +1,23 @@
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+    <h1>Mon super blog !</h1>
+    <p>Derniers billets du bloge :</p>
+
+    <form  action="index.php?action=updatePost&amp;id=<?= $_GET['id'] ?>" method="post">
+        <div>
+            <label for="title">titre</label><br />
+            <input type="text" id="title" name="title" />
+        </div>
+        <div>
+            <label for="content">Contenue</label>
+            <textarea id="content" name="content"></textarea>
+        </div>
+        <div>
+            <input type="submit" />
+        </div>
+    </form>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/frontend/template.php'); ?>
