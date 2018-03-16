959b85f05a5438a0ab67a370a64f2f9c6307df75
alex31081993 <alexis.gressier62@gmail.com>
2018-03-16 18:01:36 +0100

    add a name true

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -35,7 +35,7 @@ while ($data = $posts->fetch())
         <p>
             <?= nl2br(htmlspecialchars($data['content'])) ?>
             <br />
-            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
+            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Voir le billet</a></em>
         </p>
     </div>
 <?php
