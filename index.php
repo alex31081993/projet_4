3ca28e8ea6e8eeed53a5e010c079a055c9580fb4
alex31081993 <alexis.gressier62@gmail.com>
2018-03-26 21:14:04 +0200

    update backend for dependency injection

--- index.php
+++ index.php
@@ -3,16 +3,18 @@ session_start();
 require "autoload/Autoload.php";
 \autoload\Autoload::register();
 
+$container = new \service\Container();
+$controllerFrontEnd = $container->getControllerFrontend();
+$controllerBackEnd = $container->getControllerBackend();
+
 try {
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'listPosts') {
-            $controller = new \controller\Frontend();
-            $controller->listPosts();
+            $controllerFrontEnd->listPosts();
 
         } elseif ($_GET['action'] == 'post') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
-                $indexPost = new \controller\Frontend();
-                $indexPost->post();
+                $controllerFrontEnd->post();
 
             } else {
                 throw new Exception('Aucun identifiant de billet envoyé');
@@ -21,8 +23,7 @@ try {
         } elseif ($_GET['action'] == 'addComment') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
                 if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    $indexAddComment = new \controller\Frontend();
-                    $indexAddComment->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                    $controllerFrontEnd->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
                 } else {
                     throw new Exception('Tous les champs ne sont pas remplis !');
                 }
@@ -33,8 +34,7 @@ try {
         } elseif ($_GET['action'] == 'supContent') {
             if (isset ($_SESSION['pseudo'])) {
                 if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    $indexAddComment = new \controller\Backend();
-                    $indexAddComment->deletePost();
+                    $controllerBackEnd->deletePost();
                 } else {
                     throw new Exception('Aucun identifiant de billet envoyé');
                 }
@@ -44,8 +44,7 @@ try {
 
         } elseif ($_GET['action'] == 'updateContent') {
             if (isset ($_SESSION['pseudo'])) {
-                $controller = new \controller\Backend();
-                $controller->viewPostAdmin();
+                $controllerBackEnd->viewPostAdmin();
             } else {
                 throw new Exception('Vous n\'etes pas connecter !');
             }
@@ -53,8 +52,7 @@ try {
         } elseif ($_GET['action'] == 'updatePost') {
             if (isset ($_SESSION['pseudo'])) {
                 if (!empty($_POST['title']) && !empty($_POST['chapeau']) && !empty($_POST['content'])) {
-                    $controller = new \controller\Backend();
-                    $controller->updatePost($_POST['title'], $_POST['chapeau'], $_POST['content']);
+                    $controllerBackEnd->updatePost($_POST['title'], $_POST['chapeau'], $_POST['content']);
                 } else {
                     throw new Exception('Tous les champs ne sont pas remplis !');
                 }
@@ -63,55 +61,47 @@ try {
             }
 
         } elseif ($_GET['action'] == 'connectAdminView') {
-            $controller = new \controller\Backend();
-            $controller->viewAdminConnect();
+            $controllerBackEnd->viewAdminConnect();
 
         } elseif ($_GET['action'] == 'postAdminView') {
             if (!empty(htmlspecialchars($_POST['pseudo'])) and !empty(htmlspecialchars($_POST['pass']))) {
-                $controller = new \controller\Backend();
-                $controller->login();
+                $controllerBackEnd->login();
             } else {
                 throw new Exception('Impossilbe d\'afficher la page demander !');
             }
 
         } elseif ($_GET['action'] == 'logout') {
-            $controller = new \controller\Backend();
-            $controller->logOut();
+            $controllerBackEnd->logOut();
 
         } elseif ($_GET['action'] == 'viewAddPost') {
             if (isset ($_SESSION['pseudo'])) {
-                $controller = new \controller\Backend();
-                $controller->viewAddPost();
+                $controllerBackEnd->viewAddPost();
             } else {
                 throw new Exception('Vous etes pas autorisé a crée un post');
             }
 
         } elseif ($_GET['action'] == 'reportComment') {
             if (isset($_GET['id'])) {
-                $controller = new \controller\Backend();
-                $controller->reportComment($_GET['id']);
+                $controllerBackEnd->reportComment($_GET['id']);
             } else {
                 throw new Exception('Aucun commentaire a signalé');
             }
 
         } elseif ($_GET['action'] == 'reportCommentVerified') {
             if (isset($_GET['id'])) {
-                $controller = new \controller\Backend();
-                $controller->reportCommentVerified($_GET['id']);
+                $controllerBackEnd->reportCommentVerified($_GET['id']);
             } else {
                 throw new Exception('Aucun commentaire a signalé');
             }
         } elseif ($_GET['action'] == 'viewAdmin') {
             if (isset ($_SESSION['pseudo'])) {
-                $controller = new \controller\Backend();
-                $controller->adminView();
+                $controllerBackEnd->adminView();
             } else {
                 throw new Exception('Vous n\'etes pas connecter !');
             }
         } elseif ($_GET['action'] == 'supComment') {
             if (isset ($_SESSION['pseudo'])) {
-                $controller = new \controller\Backend();
-                $controller->deleteComment();
+                $controllerBackEnd->deleteComment();
             } else {
                 throw new Exception('vous n\'etes pas connecter !');
             }
@@ -119,8 +109,7 @@ try {
         } elseif ($_GET['action'] == 'addPost') {
             if (isset ($_SESSION['pseudo'])) {
                 if (!empty($_POST['title']) && !empty($_POST['chapeau']) && !empty($_POST['content'])) {
-                    $controller = new \controller\Backend();
-                    $controller->addPost($_POST['id'], $_POST['title'], $_POST['chapeau'], $_POST['content']);
+                    $controllerBackEnd->addPost($_POST['id'], $_POST['title'], $_POST['chapeau'], $_POST['content']);
                 } else {
                     throw new Exception('Tous les champs ne sont pas remplis !');
                 }
@@ -129,8 +118,7 @@ try {
             }
         }
     } else {
-        $indexListPosts = new \controller\Frontend();
-        $indexListPosts->listposts();
+        $controllerFrontEnd->listposts();
     }
 } catch (Exception $e) {
     echo 'Erreur : ' . $e->getMessage();
--- service/Container.php
+++ service/Container.php
@@ -2,9 +2,12 @@
 
 namespace service;
 
+use controller\Backend;
 use controller\Frontend;
 use model\CommentManager;
+use model\ConnectAdminManager;
 use model\PostManager;
+use model\PostManagerBackend;
 
 class Container {
 
@@ -21,5 +24,20 @@ class Container {
     public function getControllerFrontend()
     {
         return new Frontend($this->getPostManager(), $this->getCommentManager());
+    }
+
+    public function getConnectAdminManager()
+    {
+        return new ConnectAdminManager();
+    }
+
+    public function getPostManagerBackend()
+    {
+        return new PostManagerBackend();
+    }
+
+    public function getControllerBackend()
+    {
+        return new Backend($this->getPostManagerBackend(), $this->getCommentManager(), $this->getConnectAdminManager(), $this->getPostManager());
     }
 }
